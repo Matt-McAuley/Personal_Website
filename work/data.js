d11aaf9680aef876eab214e80004729c2b068be0
Matthew McAuley <mattwilliam618@gmail.com>
2025-03-22 22:46:33 -0400

    Adjust projects header and create date for work page

---
+++ work/data.js
@@ -0,0 +1,31 @@
+const cards =
+  [
+    {
+      "id": 1,
+      "title": "Octus",
+      "description": "(Missing).\n",
+      "image": "Octus/octus.jpg",
+      "has_demo": false,
+      "demo_link": null,
+      "card_click_link": "https://octus.com",
+      "new_tab": true,
+      "has_github": false,
+      "github_link": null,
+      "icons": ["c#.png", "postgres.png", "redis.webp", "snowflake.png"]
+    },
+    {
+      "id": 2,
+      "title": "Cornell Cup Robotics",
+      "description": "(Missing).\n",
+      "image": "Cup/cup.png",
+      "has_demo": false,
+      "demo_link": null,
+      "card_click_link": "https://cornellcuprobotics.com/",
+      "new_tab": true,
+      "has_github": false,
+      "github_link": null,
+      "icons": ["js.png", "react.png", "python.webp", "flask.png"]
+    }
+    ]
+
+export default cards;
