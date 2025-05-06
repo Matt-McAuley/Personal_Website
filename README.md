91a797c2435a41b74a2786fea23498a811fa50cd
Matthew McAuley <mattwilliam618@gmail.com>
2025-05-06 19:12:53 -0400

    Add Personal Website project

---
+++ README.md
@@ -0,0 +1,58 @@
+# Exercise Engine
+
+## Contents
+
+- [Description](#description)
+- [Online Hosting](#online-hosting)
+- [Images](#images)
+- [Authors](#authors)
+
+## Description
+
+This was a class project for **ENGRC 3025 at Cornell University** where I worked throughout the semester to develop a personal website and online portfolio.
+It is a static website that showcases my work experience, projects, and skills beyond what other mediums like LinkedIn or a Resume could portray.
+I intended to go more in depth with my descriptions rather than simply reiterating the bullet points which recruiters probably would've already seen.
+
+The website is done entirely in HTML, SCSS, and Javascript. Bootstrap was used for the navigation bar rather than building from scratch but a lot of modifications
+to the styling were done to fit my layout better. The data for the projects and work experiences are located in JS files which are then imported into functions which
+turn them into cards or timeline elements, respectively. Automating this process was a great way to ensure that the data was consistent and easy to update in the future.
+The about page contains a short summary of my background and interests, as well as a photo of me. Technologies that I've used scroll across the screen and hovering
+over them shows the name of the technology. The projects page has cards which contain a description of the project, an image carousel, relevant technologies, a link to
+the GitHub Repo, and a link to an external website if it exists. The work experience page has a timeline with all of my previous experiences. Clicking on each bullet on
+the timeline makes a card appear with the company name, role, duration of work, a description of what I did, and the technologies used. These pages can all be reached
+through the navbar, which also contains a link to my Resume in PDF form. Lastly, social media icons for my email, GitHub, and LinkedIn are in the top right corner of every
+page for easy access.
+
+This project was my first experience using SCSS which I found to be much easier to work with than CSS. I enjoyed the use of variables and also nesting selectors to make
+the code more readable and simpler to revisit. I worked on making the website responsive to different screen sizes which I had no experience with prior, and I found it to be challenging
+but rewarding. It is important to be able to view websites on mobile devices especially in today's world, but I often overlook it on other projects because it is not a requirement.
+For this website, my focus was more so on making it visually appealing instead of adding impressive functionalities, and I spent a lot of time tweaking various
+elements to get them to look the way I wanted. I traditionally enjoy adding as many functionalities as possible and put aesthetics on the back burner, but I decided
+this project would be a good opportunity to work on my design skills. I'm very proud of the way it turned out and I think it is very clean, informative, and easy to navigate.
+I used GitHub pages to host on a custom domain which was purchased from cloudflare, so that I could have a nice looking URL that is easy to remember. Overall, I think it is
+a successful portrayal of my professional identity, and it turned out much better than templates I had used in the past.
+
+## Online Hosting
+The website is hosted on [GitHub Pages](https://mattmcauley.com).
+
+## Images
+
+#### About Page
+<img src="https://mattmcauley.com/assets/projects/Personal_Website/about.png" alt="Image" style="width: 95%">
+
+#### Work Experience Page
+<img src="https://mattmcauley.com/assets/projects/Personal_Website/work.png" alt="Image" style="width: 95%">
+
+#### Projects Page
+<img src="https://mattmcauley.com/assets/projects/Personal_Website/projects.png" alt="Image" style="width: 95%">
+
+## Authors
+
+<table style="border-collapse: collapse; border:none;">
+  <tr  style="border:none;">
+    <td style="border:none;"><strong>Matthew McAuley</strong></td>
+  </tr>
+  <tr style="border:none;">
+    <td style="border:none;">mwm223@cornell.edu</td>
+  </tr>
+</table>
--- js/pages/projects/projects-data.js
+++ js/pages/projects/projects-data.js
@@ -1,7 +1,6 @@
 const cards =
   [
     {
-      "id": 1,
       "title": "Exercise Engine",
       "description": "While at the gym, you often need to swap out certain exercises due to equipment issues or physical limitations. This project sought to provide an information retrieval database so users could find exercises that targeted similar muscle groups while being able to filter by different criteria. The website was implemented using HTML, CSS, and Javascript on the frontend, with a Python Flask API on the backend for performing the similarity analysis. One major challenge was finding an exercise database with lengthy descriptions, which we solved by using Selenium to web scrape the information from bodybuilding.com. Our team successfully implemented a functional application that I use extensively in my daily life to find good fitness alternatives and avoid boredom. Click the link below to demo the website yourself or the GitHub icon to check out the repository with a more in depth description.",
       "images": ["Exercise_Engine", "home.png", "results.png", "results_filtered.png", "freeform.png"],
@@ -12,7 +11,6 @@ const cards =
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", "HTML"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", "CSS"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", "JavaScript"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", "Python"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg", "Flask"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg", "AWS"]]
     },
     {
-      "id": 2,
       "title": "GymBuddy",
       "description": "To keep track of your workout you often need many different apps such as a rest, timer, set counter, and weight tracker, which frequently have ads or features locked behind paywalls. This project consolidates the main functionalities of these apps into one for a streamlined gym experience. The mobile app was developed using the Expo and React-Native frameworks with Typescript as the programming language. One major challenge was dealing with memory limitations on iOS which I solved using the LocalStorage API to create the illusion of resuming even when the app forcibly restarted. I use this app nearly every day in the gym to increase my efficiency and enjoyment and it is a project I am extremely proud of. Click the GitHub icon to check out the repository with a more in depth description.",
       "images": ["GymBuddy", "splash.png", "home_nav.png", "home.png", "programs.png", "edit_program.png", "days.png", "add_day.png", "exercises.png", "add_exercise.png", "music.png", "playlists.png"],
@@ -23,7 +21,6 @@ const cards =
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg", "TypeScript"], ["/assets/icons/expo.svg", "Expo"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg", "React"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg", "SQLite"], ["/assets/icons/phone.svg", "Mobile"]]
     },
     {
-      "id": 3,
       "title": "To-Do Manager",
       "description": "In college (and life in general) it can be difficult to keep track of all of your required deadlines and responsibilities. This project was designed mainly to gain experience with modern full-stack web development technologies while also creating an application that I could use to organize my daily life. It allows users to create tasks with descriptions, priorities, and due dates that are organized into various projects. The frontend was created using React and Typescript and the backend is a Python Flask API with a Sqlite DB. One major obstacle I had to overcome was the false intuition to mutate React state which wasn’t effectively reflecting on the page. Through reading the documentation, I came to understand how state should be treated as immutable. This project was a great learning experience which reinforced my understanding of modern web dev technologies, version control, and writing maintainable code. Click the link below to demo the website yourself or the GitHub icon to check out the repository with a more in depth description.",
       "images": ["To-Do_Manager", "home.png", "login.png", "signup.png", "form.png"],
@@ -34,7 +31,6 @@ const cards =
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg", "TypeScript"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg", "React"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", "Python"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flask/flask-original.svg", "Flask"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlalchemy/sqlalchemy-original.svg", "SQLAlchemy"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sqlite/sqlite-original.svg", "SQLite"]]
     },
     {
-      "id": 4,
       "title": "Caml Capital",
       "description": "Board games are a great pastime but often require materials which can be expensive or tedious to set up. This project was created to gain experience with functional programming and to develop a virtual recreation of the board game Monopoly that can be easily played with just a computer. The terminal recreation was done entirely in OCaml with very minimal mutable state. One major challenge was creating a game loop without reassigning variables or being able to use OOP principles which we overcame through writing extensive documentation and well planned out code. Working with an entirely new programming paradigm than I was used to was not easy but it taught me about adaptability especially in a collaborative environment. Click the GitHub icon to check out the repository with a more in depth description.",
       "images": ["Caml_Capital", "start.png", "buying.png", "paying.png", "houses.png", "chance.png"],
@@ -45,7 +41,6 @@ const cards =
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/ocaml/ocaml-original.svg", "OCaml"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg", "Git"], ["/assets/icons/controller.svg", "Game Dev"]]
     },
     {
-      "id": 5,
       "title": "Embedded Jump",
       "description": "This project was created to better understand low level programming and interacting with microcontrollers such as the FRDM-KL46Z. We developed a recreation of the popular mobile game ‘Doodle Jump’ which was controlled using the hardware on the board. The accelerometer controls horizontal movement, while the buttons deal with shooting obstacles. The game recreation was done with Python using the Pygame library and the functions related to movement and controls were coded in C. One major challenge was communicating between the Python application running on a laptop on the C application running on the board which we solved through UART serial communication libraries. I learned about game development with Python as well as the features unique to a low level application like interrupts, pointers, and memory allocation. Click the link below to see our team’s write up on the project.",
       "images": ["Embedded_Jump", "diagram.png", "start.png", "shooting.png", "enemies.png", "over.png", "score.png"],
@@ -56,18 +51,16 @@ const cards =
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg", "C"], ["/assets/icons/microcontroller.svg", "Embedded"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg", "Python"], ["/assets/icons/controller.svg", "Game Dev"]]
     },
     {
-      "id": 6,
-      "title": "Calculator",
-      "description": "This project is a simple calculator app in the browser that supports basic arithmetic operations such as addition, subtraction, division, and multiplication. It was created using HTML, CSS, and Javascript. It was one of my first introductions to the world of web development and frontend technologies. Click the GitHub icon to check out the repository with a more in depth description.",
-      "images": ["Calculator", "home.png", "number.png", "zero.png", "overflow.png"],
+      "title": "Personal Website",
+      "description": "Professional mediums like LinkedIn and Resumes are often limited in their portrayal of a candidate to recruiters. This project was created to give more information regarding my projects, previous work experience, and overall professional identity to anyone interested and provide a living timeline of what I’ve accomplished thus far. The website was implemented using HTML, SCSS, and Javascript on the frontend, with some Bootstrap templating used for the navbar. It is hosted on GitHub pages on a custom domain purchased from Cloudflare. I was able to design an online portfolio that is very informative, aesthetically pleasing, and easy to navigate which can provide others a more holistic view of who I am and what I offer. Developing this project also reinforced my understanding of web development basics as well as taught me about new ideas such as responsive design. Click the GitHub icon to check out the repository with a more in depth description.",
+      "images": ["Personal_Website", "about.png", "work.png", "projects.png", "projects2.png"],
       "has_demo": true,
-      "demo_link": "https://matt-mcauley.github.io/Calculator",
+      "demo_link": "https://mattmcauley.com",
       "has_github": true,
-      "github_link": "https://github.com/matt-mcauley/Calculator",
-      "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", "HTML"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", "CSS"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", "JavaScript"]]
+      "github_link": "https://github.com/matt-mcauley/Personal_Website",
+      "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", "HTML"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg", "Sass"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", "JavaScript"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg", "Bootstrap"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cloudflare/cloudflare-original.svg", "Cloudflare"]]
     },
     {
-      "id": 7,
       "title": "Etch-A-Sketch",
       "description": "This project is a drawing canvas in the browser that supports resizing, erasing, changing drawing color, and using progressive darkening. It was created using HTML, CSS, and Javascript. It was one of my first introductions to the world of web development and frontend technologies. Click the GitHub icon to check out the repository with a more in depth description.",
       "images": ["Etch-A-Sketch", "home.png", "progressive.png", "solid.png", "zoomout.png", "zoomin.png"],
@@ -75,6 +68,16 @@ const cards =
       "demo_link": "https://matt-mcauley.github.io/Etch-A-Sketch",
       "has_github": true,
       "github_link": "https://github.com/matt-mcauley/Etch-A-Sketch",
+      "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", "HTML"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", "CSS"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", "JavaScript"]]
+    },
+    {
+      "title": "Calculator",
+      "description": "This project is a simple calculator app in the browser that supports basic arithmetic operations such as addition, subtraction, division, and multiplication. It was created using HTML, CSS, and Javascript. It was one of my first introductions to the world of web development and frontend technologies. Click the GitHub icon to check out the repository with a more in depth description.",
+      "images": ["Calculator", "home.png", "number.png", "zero.png", "overflow.png"],
+      "has_demo": true,
+      "demo_link": "https://matt-mcauley.github.io/Calculator",
+      "has_github": true,
+      "github_link": "https://github.com/matt-mcauley/Calculator",
       "icons": [["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg", "HTML"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg", "CSS"], ["https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg", "JavaScript"]]
     }
 ];
